57eb3f3ba871d0c32b4368f0b9719c3fcf1a7906
Burak Karakan <burak.karakan@gmail.com>
2022-02-03 23:19:15 +0100

    start printing the issue context as well

--- pkg/lint/print.go
+++ pkg/lint/print.go
@@ -10,14 +10,21 @@ import (
 
 type Printer struct{}
 
+type taskSummary struct {
+	rule   *Rule
+	issues []*Issue
+}
+
+var (
+	faint           = color.New(color.Faint).SprintFunc()
+	successPrinter  = color.New(color.FgGreen)
+	pipelinePrinter = color.New(color.FgBlue, color.Bold)
+	taskNamePrinter = color.New(color.FgYellow, color.Bold)
+	issuePrinter    = color.New(color.FgRed)
+	contextPrinter  = color.New(color.FgRed, color.Concealed)
+)
+
 func (l *Printer) PrintIssues(analysis *PipelineAnalysisResult) {
-	successPrinter := color.New(color.FgGreen)
-	pipelinePrinter := color.New(color.FgBlue, color.Bold)
-	taskNamePrinter := color.New(color.FgYellow, color.Bold)
-	issuePrinter := color.New(color.FgRed)
-
-	faint := color.New(color.Faint).SprintFunc()
-
 	for _, pipelineIssues := range analysis.Pipelines {
 		successPrinter.Println()
 
@@ -29,17 +36,12 @@ func (l *Printer) PrintIssues(analysis *PipelineAnalysisResult) {
 			continue
 		}
 
-		type taskSummary struct {
-			rule   *Rule
-			issues []*Issue
-		}
-
-		taskIssueMap := make(map[*pipeline.Task]taskSummary)
+		taskIssueMap := make(map[*pipeline.Task]*taskSummary)
 
 		for rule, issues := range pipelineIssues.Issues {
 			for _, issue := range issues {
 				if _, ok := taskIssueMap[issue.Task]; !ok {
-					taskIssueMap[issue.Task] = taskSummary{
+					taskIssueMap[issue.Task] = &taskSummary{
 						rule:   rule,
 						issues: []*Issue{},
 					}
@@ -48,7 +50,7 @@ func (l *Printer) PrintIssues(analysis *PipelineAnalysisResult) {
 				taskIssues := taskIssueMap[issue.Task].issues
 				taskIssues = append(taskIssues, issue)
 
-				taskIssueMap[issue.Task] = taskSummary{
+				taskIssueMap[issue.Task] = &taskSummary{
 					rule:   rule,
 					issues: taskIssues,
 				}
@@ -57,17 +59,39 @@ func (l *Printer) PrintIssues(analysis *PipelineAnalysisResult) {
 
 		for task, summary := range taskIssueMap {
 			taskNamePrinter.Printf("  %s (%s)\n", task.Name, task.DefinitionFile.Path)
-			issueCount := len(summary.issues)
-			for index, issue := range summary.issues {
-				connector := "├──"
-				if index == issueCount-1 {
-					connector = "└──"
-				}
-
-				issuePrinter.Printf("    %s %s %s\n", connector, issue.Description, faint(fmt.Sprintf("(%s)", summary.rule.Name)))
-			}
+			printTaskSummary(summary)
 
 			issuePrinter.Println()
 		}
+	}
+}
+
+func printTaskSummary(summary *taskSummary) {
+	issueCount := len(summary.issues)
+	for index, issue := range summary.issues {
+		connector := "├──"
+		if index == issueCount-1 {
+			connector = "└──"
+		}
+
+		issuePrinter.Printf("    %s %s %s\n", connector, issue.Description, faint(fmt.Sprintf("(%s)", summary.rule.Name)))
+		printIssueContext(issue.Context, index == issueCount-1)
+	}
+}
+
+func printIssueContext(context []string, lastIssue bool) {
+	issueCount := len(context)
+	beginning := "│"
+	if lastIssue {
+		beginning = " "
+	}
+
+	for index, row := range context {
+		connector := "├─"
+		if index == issueCount-1 {
+			connector = "└─"
+		}
+
+		contextPrinter.Printf("    %s   %s %s\n", beginning, connector, row)
 	}
 }
