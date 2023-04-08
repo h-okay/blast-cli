fa7425854e488563481195dc8b38ac932362b7f4
Burak Karakan <burak.karakan@gmail.com>
2023-04-08 21:33:36 +0100

    add unit tests for render

--- cmd/render.go
+++ cmd/render.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
 	"github.com/alecthomas/chroma/v2/quick"
 	"github.com/datablast-analytics/blast/pkg/bigquery"
 	"github.com/datablast-analytics/blast/pkg/executor"
+	"github.com/datablast-analytics/blast/pkg/pipeline"
 	"github.com/datablast-analytics/blast/pkg/query"
 	"github.com/urfave/cli/v2"
 )
@@ -18,62 +20,93 @@ func Render() *cli.Command {
 		Usage:     "render a single Blast SQL asset",
 		ArgsUsage: "[path to the asset file]",
 		Action: func(c *cli.Context) error {
-			taskPath := c.Args().Get(0)
-			if taskPath == "" {
-				errorPrinter.Printf("Please give an asset path to render: blast-cli render <path to the asset file>)\n")
-				return cli.Exit("", 1)
+			r := RenderCommand{
+				extractor: &query.WholeFileExtractor{
+					Fs:       fs,
+					Renderer: query.DefaultJinjaRenderer,
+				},
+				bqMaterializer: &bigquery.Materializer{},
+				builder:        builder,
+				writer:         os.Stdout,
 			}
 
-			task, err := builder.CreateTaskFromFile(taskPath)
-			if err != nil {
-				errorPrinter.Printf("Failed to build asset: %v\n", err.Error())
-				return cli.Exit("", 1)
-			}
-
-			if task == nil {
-				errorPrinter.Printf("The given file path doesn't seem to be a Blast asset definition: '%s'\n", taskPath)
-				return cli.Exit("", 1)
-			}
-
-			wholeFileExtractor := &query.WholeFileExtractor{
-				Fs:       fs,
-				Renderer: query.DefaultJinjaRenderer,
-			}
-
-			queries, err := wholeFileExtractor.ExtractQueriesFromFile(task.ExecutableFile.Path)
-			if err != nil {
-				errorPrinter.Printf("Failed to extract queries from file: %v\n", err.Error())
-				return cli.Exit("", 1)
-			}
-
-			qq := queries[0]
-
-			if task.Type == executor.TaskTypeBigqueryQuery {
-				materializer := bigquery.Materializer{}
-				materialized, err := materializer.Render(task, qq.Query)
-				if err != nil {
-					errorPrinter.Printf("Failed to materialize the query: %v\n", err.Error())
-					return cli.Exit("", 1)
-				}
-
-				qq.Query = materialized
-				qq.Query = highlightCode(qq.Query, "sql")
-			}
-
-			fmt.Printf("%s\n", qq)
-
-			return nil
+			return r.Run(c.Args().Get(0))
 		},
 	}
 }
 
+type queryExtractor interface {
+	ExtractQueriesFromFile(path string) ([]*query.Query, error)
+}
+
+type queryMaterializer interface {
+	Render(task *pipeline.Asset, query string) (string, error)
+}
+
+type taskCreator interface {
+	CreateTaskFromFile(path string) (*pipeline.Asset, error)
+}
+
+type RenderCommand struct {
+	extractor      queryExtractor
+	bqMaterializer queryMaterializer
+	builder        taskCreator
+
+	writer io.Writer
+}
+
+func (r *RenderCommand) Run(taskPath string) error {
+	if taskPath == "" {
+		errorPrinter.Printf("Please give an asset path to render: blast-cli render <path to the asset file>)\n")
+		return cli.Exit("", 1)
+	}
+
+	task, err := r.builder.CreateTaskFromFile(taskPath)
+	if err != nil {
+		errorPrinter.Printf("Failed to build asset: %v\n", err.Error())
+		return cli.Exit("", 1)
+	}
+
+	if task == nil {
+		errorPrinter.Printf("The given file path doesn't seem to be a Blast asset definition: '%s'\n", taskPath)
+		return cli.Exit("", 1)
+	}
+
+	queries, err := r.extractor.ExtractQueriesFromFile(task.ExecutableFile.Path)
+	if err != nil {
+		errorPrinter.Printf("Failed to extract queries from file: %v\n", err.Error())
+		return cli.Exit("", 1)
+	}
+
+	qq := queries[0]
+
+	if task.Type == executor.TaskTypeBigqueryQuery {
+		materialized, err := r.bqMaterializer.Render(task, qq.Query)
+		if err != nil {
+			errorPrinter.Printf("Failed to materialize the query: %v\n", err.Error())
+			return cli.Exit("", 1)
+		}
+
+		qq.Query = materialized
+		qq.Query = highlightCode(qq.Query, "sql")
+	}
+
+	_, err = r.writer.Write([]byte(fmt.Sprintf("%s\n", qq)))
+
+	return err
+}
+
 func highlightCode(code string, language string) string {
-	o, _ := os.Stdout.Stat()
+	o, err := os.Stdout.Stat()
+	if err != nil {
+		return code
+	}
+
 	if (o.Mode() & os.ModeCharDevice) != os.ModeCharDevice {
 		return code
 	}
 	b := new(strings.Builder)
-	err := quick.Highlight(b, code, language, "terminal16m", "monokai")
+	err = quick.Highlight(b, code, language, "terminal16m", "monokai")
 	if err != nil {
 		errorPrinter.Printf("Failed to highlight the query: %v\n", err.Error())
 		return code
